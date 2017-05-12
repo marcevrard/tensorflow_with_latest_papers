4f16c4a9bf8a8b1a1d4fb2a48e7884603e9639af
Marc Evrard <marc.evrard@gmail.com>
2017-05-12 13:14:45 +0900

    Manually apply other pep8 fixes

--- rnn_cell_modern.py
+++ rnn_cell_modern.py
@@ -15,7 +15,8 @@ from multiplicative_integration_modern import multiplicative_integration
 from normalization_ops_modern import layer_norm
 from six.moves import xrange
 
-# from multiplicative_integration import multiplicative_integration, multiplicative_integration_for_multiple_inputs
+# from multiplicative_integration import (multiplicative_integration,
+#                                         multiplicative_integration_for_multiple_inputs)
 
 
 RNNCell = rnn_cell.RNNCell
@@ -177,11 +178,13 @@ class MGUCell(RNNCell):
 class LSTMCell_MemoryArray(RNNCell):
     '''Implementation of Recurrent Memory Array Structures Kamil Rocki
     https://arxiv.org/abs/1607.03085
-    Idea is to build more complex memory structures within one single layer rather than stacking multiple layers of RNNs
+    Idea is to build more complex memory structures within one single layer
+    rather than stacking multiple layers of RNNs.
     '''
 
-    def __init__(self, num_units, num_memory_arrays=2, use_multiplicative_integration=True, use_recurrent_dropout=False, recurrent_dropout_factor=0.90, is_training=True, forget_bias=1.0,
-                 use_layer_normalization=False):
+    def __init__(self, num_units, num_memory_arrays=2, use_multiplicative_integration=True,
+                 use_recurrent_dropout=False, recurrent_dropout_factor=0.90, is_training=True,
+                 forget_bias=1.0, use_layer_normalization=False):
         self._num_units = num_units
         self.num_memory_arrays = num_memory_arrays
         self.use_multiplicative_integration = use_multiplicative_integration
@@ -260,9 +263,11 @@ class LSTMCell_MemoryArray(RNNCell):
 
 
 class JZS1Cell(RNNCell):
-    '''Mutant 1 of the following paper: http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf'''
+    '''Mutant 1 of the following paper:
+    http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf'''
 
-    def __init__(self, num_units, gpu_for_layer=0, weight_initializer="uniform_unit", orthogonal_scale_factor=1.1):
+    def __init__(self, num_units, gpu_for_layer=0,
+                 weight_initializer="uniform_unit", orthogonal_scale_factor=1.1):
         self._num_units = num_units
         self._gpu_for_layer = gpu_for_layer
         self._weight_initializer = weight_initializer
@@ -289,12 +294,16 @@ class JZS1Cell(RNNCell):
                     # We start with bias of 1.0 to not reset and not update.
                     ## equation 1 z = sigm(WxzXt+Bz), x_t is inputs
                     z = tf.sigmoid(linear([inputs],
-                                          self._num_units, True, 1.0, weight_initializer=self._weight_initializer, orthogonal_scale_factor=self._orthogonal_scale_factor))
+                                          self._num_units, True, 1.0,
+                                          weight_initializer=self._weight_initializer,
+                                          orthogonal_scale_factor=self._orthogonal_scale_factor))
 
                 with tf.variable_scope("Rinput"):
                     ## equation 2 r = sigm(WxrXt+Whrht+Br), h_t is the previous state
                     r = tf.sigmoid(linear([inputs, state],
-                                          self._num_units, True, 1.0, weight_initializer=self._weight_initializer, orthogonal_scale_factor=self._orthogonal_scale_factor))
+                                          self._num_units, True, 1.0,
+                                          weight_initializer=self._weight_initializer,
+                                          orthogonal_scale_factor=self._orthogonal_scale_factor))
                 ## equation 3
                 with tf.variable_scope("Candidate"):
                     component_0 = linear([r * state],
@@ -310,9 +319,11 @@ class JZS1Cell(RNNCell):
 
 
 class JZS2Cell(RNNCell):
-    '''Mutant 2 of the following paper: http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf'''
+    '''Mutant 2 of the following paper:
+    http://www.jmlr.org/proceedings/papers/v37/jozefowicz15.pdf'''
 
-    def __init__(self, num_units, gpu_for_layer=0, weight_initializer="uniform_unit", orthogonal_scale_factor=1.1):
+    def __init__(self, num_units, gpu_for_layer=0,
+                 weight_initializer="uniform_unit", orthogonal_scale_factor=1.1):
         self._num_units = num_units
         self._gpu_for_layer = gpu_for_layer
         self._weight_initializer = weight_initializer
@@ -338,9 +349,9 @@ class JZS2Cell(RNNCell):
                     ## equation 1
                     z = tf.sigmoid(
                         linear([inputs, state],
-                                self._num_units, True, 1.0,
-                                weight_initializer=self._weight_initializer,
-                                orthogonal_scale_factor=self._orthogonal_scale_factor))
+                               self._num_units, True, 1.0,
+                               weight_initializer=self._weight_initializer,
+                               orthogonal_scale_factor=self._orthogonal_scale_factor))
 
                 ## equation 2
                 with tf.variable_scope("Rinput"):
@@ -442,11 +453,10 @@ class Delta_RNN(RNNCell):
         '''Simulates Equation 3 in Delta RNN paper
         r, the gate, can be parameterized in many different ways.
         '''
-
-        assert inner_function_output.get_shape().as_list(
-        ) == past_hidden_state.get_shape().as_list()
-        r = tf.get_variable("outer_function_gate", [
-                            self._num_units], dtype=tf.float32, initializer=tf.zeros_initializer)
+        assert (inner_function_output.get_shape().as_list() ==
+                past_hidden_state.get_shape().as_list())
+        r = tf.get_variable("outer_function_gate", [self._num_units], dtype=tf.float32,
+                            initializer=tf.zeros_initializer)
 
         # Equation 5 in Delta Rnn Paper
         if wx_parameterization_gate:
